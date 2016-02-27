0070c05e1e870b1f7c0aa1faecff32699b5b519c
Anupam Mishra <anupamish@yahoo.com>
2016-02-27 22:28:18 +0530

    Initial Commit

---
+++ views/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Login into Student Result Processing System</title>
+    
+    
+    
+    
+        <link rel="stylesheet" href="http://localhost/studentMarks/css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <body>
+	<div class="login">
+		<div class="login-screen">
+			<div class="app-title">
+			<h2>Student Result Processing System</h2>
+			</div>
+			<div class="login-form">
+			<img height="154" width="155" align="center" src="http://localhost://studentMarks/imageResources/gbu.png">
+			</div>
+
+			<div class="login-form">
+				<div class="control-group">
+				<input type="text" class="login-field" value="" placeholder="Username" id="login-name">
+				<label class="login-field-icon fui-user" for="login-name"></label>
+				</div>
+
+				<div class="control-group">
+				<input type="password" class="login-field" value="" placeholder="Password" id="login-pass">
+				<label class="login-field-icon fui-lock" for="login-pass"></label>
+				</div>
+
+				<a class="btn btn-primary btn-large btn-block" href="#">Login</a>
+				<br>
+				<a class="btn1 btn-primary btn-large btn-block" href="#">Register Now</a>
+							</div>
+		</div>
+	</div>
+</body>
+    
+    
+    
+    
+    
+  </body>
+</html>
