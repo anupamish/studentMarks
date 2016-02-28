52e937791aed5fd5131d458fcdca6920e6917473
shubh453 <shubh.shukla17@gmail.com>
2016-02-28 15:21:47 +0530

    Commit #13
    Made Password Recovery form.

---
+++ phpIncludes/resetPassword.php
@@ -0,0 +1,48 @@
+<?php session_start();
+include ('connectMysql.php'); //connects to the database
+if (isset($_POST['email'])){
+    $username = $_POST['email'];
+    $sql_query="select * from users where email='$username'";
+    $result   = $link->query($sql_query);
+    $count= mysqli_num_rows($result);
+    // If the count is equal to one, we will send message other wise display an error message.
+    if($count==1)
+    {
+        $rows=mysqli_fetch_array($result);
+        $pass  =  $rows['password'];//FETCHING PASS
+        //echo "your pass is ::".($pass)."";
+        $to = $rows['email'];
+        //echo "your email is ::".$email;
+        //Details for sending E-mail
+        $from = "SMPS Admin";
+        $url = "http://www.gmail.com/";
+        $body  =  "SMPS password recovery Script
+        -----------------------------------------------
+        URL: $url;
+        E-Mail: $to;
+        Your password  : $pass;
+        Sincerely,
+        Student Marks Processing System";
+        $from = "shubh.shukla17@gmail.com";
+        $subject = "Student Marks Processing System Password recovery.";
+        $headers1 = "From: $from\n";
+        $headers1 .= "Content-type: text/html;charset=iso-8859-1\r\n";
+        $sentMail = mail($to,$subject,$body,$headers1);
+    } else {
+    if ($_POST ['email'] != "") {
+    echo " Not found your email in our database";
+        }
+        }
+    //If the message is sent successfully, display sucess message otherwise display an error message.
+    if($sentMail)
+    {
+        echo "<span style='color: #ff0000;'> Your Password Has Been Sent To Your Email Address.</span>";
+    }
+        else
+        {
+        if($_POST['email']!="")
+        echo "<span style='color: #ff0000;'> Cannot send password to your e-mail address.Problem with sending mail...</span>";
+    }
+
+}
+?>
---
+++ views/forgetPassword.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Login into Student Result Processing System</title>   
+        <link rel="stylesheet" href="http://localhost/studentMarks/css/styleLogin.css">
+  </head>
+
+  <body>
+
+    <body>
+    <div class="login">
+        <div class="login-screen">
+            <div class="app-title">
+            <h2>Student Result Processing System</h2>
+            </div>
+            <div class="login-form">
+            <img height="154" width="155" align="center" src="http://localhost://studentMarks/imageResources/gbu.png">
+            </div>
+            <form action="http://localhost/studentmarks/phpIncludes/resetPassword.php" method="post">
+            <div class="login-form">
+                <div class="control-group">
+                <input type="text" class="login-field" value="" placeholder="Email" name ="email">
+                <label class="login-field-icon fui-user" for="login-name"></label>
+                </div>
+            
+                <div class= "control-group">
+                <button type="submit" class="btn btn-primary btn-large btn-block">Recover Password </button>
+                </div>
+                            </div>
+                            </form>
+        </div>
+    </div>
+   </body>
+  
+  </body>
+</html>
